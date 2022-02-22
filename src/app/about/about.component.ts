238f657d51eb7fce664ef30e5f8f8ddea73101d9
AkankshaRam <akanksharamgirwar@gmail.com>
2022-02-22 11:56:45 +0530

    CRM PROJECT

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+
+import { MatCard } from "@angular/material/card";
+
+const newLocal: string = `
+    .title {
+      font-weight:bold;
+      font-size: 24px;
+    }
+    .about-card {
+      display: grid;
+      justify-content: center;
+      text-align: center;
+    }
+    .subheader{
+      font-size: 46px;
+      color:darkcyan;
+      padding-top: 50px;
+      padding-bottom: 50px
+    }
+    .content {
+      padding: 26px;
+      font-size: 18px;
+      text-align: left;
+      line-height: 2em;
+    }
+  `;
+
+@Component({
+  
+  selector: 'about',
+  styleUrls: ['./about.component.scss'],
+  templateUrl: './about.component.html'
+})
+export class AboutComponent implements OnInit {
+  public localState: any;
+  pageTitle: string = "About";
+
+  constructor(public route: ActivatedRoute) { }
+
+  public ngOnInit() {
+    this.route.data.subscribe((data: any) => {
+      /**
+       * Your resolved data from route.
+       */
+      this.localState = data.yourData;
+    });
+
+  }
+
+}
